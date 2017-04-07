3b82a26066d60495a398fdffc134f3b8b6e73dca
Kevin Foster <none@none>
2017-04-07 18:40:07 -0400

    Updated song show erb

--- app/models/field_category.rb
+++ app/models/field_category.rb
@@ -1,3 +1,4 @@
 class FieldCategory < ApplicationRecord
   has_many :fields
+  has_many :songs, :through => :fields
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :user
   has_many :fields
+  has_many :field_categories, :through => :fields
   
   # ensure that a user_id is present
   validates :user_id, presence: true
