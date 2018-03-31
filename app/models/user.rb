8f48ddddc92e727f0bb7bb36f83fd8d8fe07e2d7
Kevin Foster <none@none>
2018-03-30 21:56:57 -0400

    Adding ability to lock and reset accounts / passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         #:recoverable, :rememberable, :trackable, :validatable
-         :rememberable, :validatable, :invite_for => 2.weeks
+  # :confirmable, :timeoutable and :omniauthable,  :trackable,
+  devise  :invitable, :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable, :lockable,
+          :invite_for => 2.weeks
   has_many :songs
   has_many :sources
 end
