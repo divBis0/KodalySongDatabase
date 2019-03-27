345e437c49cd852168a82939fbff4b164ae8bf53
Kevin Foster <none@none>
2019-03-27 15:57:21 -0400

    Setting up mailgun for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,12 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: ENV['APP_DOMAIN'],
+    authentication: "plain"
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
