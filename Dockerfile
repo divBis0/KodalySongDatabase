9144495ceac75c69e5a766a9105c643d9020636b
Kevin Foster <none@none>
2020-06-28 22:47:18 -0400

    Bumped rails to 5.2 correctly

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM heroku/heroku:18-build
 RUN apt-get update -qq && apt-get install -y nodejs
 RUN gem install bundler
+RUN gem update --system 3.0.6
 RUN mkdir /app
 ADD Gemfile /app/Gemfile
 ADD Gemfile.lock /app/Gemfile.lock
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,9 +3,21 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
+# Allow overriding of the sprockets cache path
+Rails.application.config.assets.configure do |env|
+  env.cache = Sprockets::Cache::FileStore.new(
+    ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
+    Rails.application.config.assets.cache_limit,
+    env.logger
+  )
+end
+
+# Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
